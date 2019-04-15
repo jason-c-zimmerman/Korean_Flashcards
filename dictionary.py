dcc4bf3de2a40127f0a3f929562e801868cf6165
jason <jason@jason.com>
2019-04-15 00:42:32 -0700

    initial dictionary, chapters 1&2

---
+++ dictionary.py
@@ -0,0 +1,339 @@
+# -*- coding: utf-8 -*-
+
+dictionary = [[\
+        #Chapter 1\
+        {\
+        'english':'Yes.',\
+        'explanation':'',\
+        'korean':'네 or 예',\
+        'audio_path':'audio/chapter1/01.mp3'\
+        },{\
+        'english':'No.',\
+        'explanation':'',\
+        'korean':'아니오',\
+        'audio_path':'audio/chapter1/02.mp3'\
+        },{\
+        'english':'How are you, Mr. Kim?',\
+        'explanation':'Explanation: 김 (a family name); 선냉 teacher, Mr. or Mrs. or Ms.; 선생님 revered teacher, Mr., Mrs. or Ms.; 안녕하세요? Are you peaceful (well)?',\
+        'korean':'김선생님, 안녕하세요?',\
+        'audio_path':'audio/chapter1/03.mp3'\
+        },{\
+        'english':'Fine, how are you? ',\
+        'explanation':'',\
+        'korean':'네. 안녕하세요?',\
+        'audio_path':'audio/chapter1/04.mp3'\
+        },{\
+        'english':'I’m . . .',\
+        'explanation':'',\
+        'korean':'. . .  입니다.',\
+        'audio_path':'audio/chapter1/05.mp3'\
+        },{\
+        'english':'Nice to meet you; nice to see you. Literally: I meet you, so I am pleased.',\
+        'explanation':'',\
+        'korean':'만나서 반갑습니다.',\
+        'audio_path':'audio/chapter1/06.mp3'\
+        },{\
+        'english':'Good-bye! To one who is leaving. Literally: Go in peace (i.e., health)',\
+        'explanation':'',\
+        'korean':'안녕히 가세요.',\
+        'audio_path':'audio/chapter1/07.mp3'\
+        },{\
+        'english':'Good-bye! To one who is staying. Literally: Stay in peace (i.e. health).',\
+        'explanation':'',\
+        'korean':'안녕히 계세요.',\
+        'audio_path':'audio/chapter1/08.mp3'\
+        },{\
+        'english':'Hello! (to someone working)',\
+        'explanation':'Explanation: 수오 hard work',\
+        'korean':'수고하십니다.',\
+        'audio_path':'audio/chapter1/09a.mp3'\
+        },{\
+        'english':'Good-bye! (to someone working)',\
+        'explanation':'Explanation: 수오 hard work',\
+        'korean':'수고하세요.',\
+        'audio_path':'audio/chapter1/09b.mp3'\
+        },{\
+        'english':'Thank you for helping me or Well done!',\
+        'explanation':'Explanation: 수오 hard work, i.e., You’re doing a great job; Keep up the good work; and Well done.',\
+        'korean':'수고하셨어요.',\
+        'audio_path':'audio/chapter1/09c.mp3'\
+        },{\
+        'english':'Pleased to make your acquaintance. Literally: I see you for the first time (formally) i.e., How do you do?',\
+        'explanation':'',\
+        'korean':'처음 뵙겠습니다.',\
+        'audio_path':'audio/chapter1/10.mp3'\
+        },{\
+        'english':'See you later! (FORMAL)',\
+        'explanation':'Explanation: 또 again,  뵙겠습니다 will humbly see/meet. Literally: I will humbly see you again.',\
+        'korean':'또 뵙겠습니다.',\
+        'audio_path':'audio/chapter1/11.mp3'\
+        },{\
+        'english':'Thank you.',\
+        'explanation':'',\
+        'korean':'고맙습니다.',\
+        'audio_path':'audio/chapter1/12a.mp3'\
+        },{\
+        'english':'Thank you.',\
+        'explanation':'',\
+        'korean':'감사합니다.',\
+        'audio_path':'audio/chapter1/12b.mp3'\
+        },{\
+        'english':'You’re welcome! or Don’t mention it! Literally: It’s one of ten million (word).',\
+        'explanation':'',\
+        'korean':'천만에요',\
+        'audio_path':'audio/chapter1/13a.mp3'\
+        },{\
+        'english':'You’re welcome! or Don’t mention it! Literally: It’s one of ten million (word).',\
+        'explanation':'',\
+        'korean':'괜찮아요',\
+        'audio_path':'audio/chapter1/13b.mp3'\
+        },{\
+        'english':'Welcome! Literally: Come (in) right away!',\
+        'explanation':'',\
+        'korean':'어서 오세요!',\
+        'audio_path':'audio/chapter1/14.mp3'\
+        },{\
+        'english':'Come in! Literally: Please enter.',\
+        'explanation':'',\
+        'korean':'들어오세요!',\
+        'audio_path':'audio/chapter1/15.mp3'\
+        },{\
+        'english':'Please take a seat / sit down.',\
+        'explanation':'',\
+        'korean':'앉으세요.',\
+        'audio_path':'audio/chapter1/16.mp3'\
+        },{\
+        'english':'Excuse me (for what I am doing). Literally: I am committing a discourtesy.',\
+        'explanation':'',\
+        'korean':'실례합니다.',\
+        'audio_path':'audio/chapter1/17a.mp3'\
+        },{\
+        'english':'Excuse me (for what I did). Literally: I have committed a discourtesy.',\
+        'explanation':'',\
+        'korean':'실례했습니다.',\
+        'audio_path':'audio/chapter1/17b.mp3'\
+        },{\
+        'english':'Excuse me (for what I’m about to do). Literally: I am about to commit a discourtesy.',\
+        'explanation':'',\
+        'korean':'시례하겠습니다.',\
+        'audio_path':'audio/chapter1/17c.mp3'\
+        },{\
+        'english':'I’m sorry or Excuse me. Literally: I feel uneasy.',\
+        'explanation':'',\
+        'korean':'미악합니다',\
+        'audio_path':'audio/chapter1/18a.mp3'\
+        },{\
+        'english':'I’m sorry or Excuse me. Literally: I feel uneasy.',\
+        'explanation':'',\
+        'korean':'죄송합니다',\
+        'audio_path':'audio/chapter1/18b.mp3'\
+        },{\
+        'english':'Not at all, it’s all right; No, thanks.',\
+        'explanation':'Explanation: 아니오 No; 괜찮아요 It makes no difference, it doesn’t matter, it’s okay.',\
+        'korean':'아니오, 괜찮아요.',\
+        'audio_path':'audio/chapter1/19.mp3'\
+        },{\
+        'english':'Hello! or Hey there! Hello on the telephone, or when peering into a dark house. Also means Look here!',\
+        'explanation':'',\
+        'korean':'여보세요!',\
+        'audio_path':'audio/chapter1/20.mp3'\
+        },{\
+        'english':'It’s time (to begin or stop).',\
+        'explanation':'Explanation: 시간 time; 시간이 time (as subject); 다 all, completely; 됐습니다 it has become . . .',\
+        'korean':'시간이 다 됐습니다.',\
+        'audio_path':'audio/chapter1/21.mp3'\
+        },{\
+        'english':'See you later! (POLITE) Literally: See you again.',\
+        'explanation':'',\
+        'korean':'또봐요.',\
+        'audio_path':'audio/chapter1/22.mp3'\
+        },{\
+        'english':'Is that so? Really? That’s so. Really.',\
+        'explanation':'',\
+        'korean':'그래요? 그래요.',\
+        'audio_path':'audio/chapter1/23.mp3'\
+        }\
+        ],[\
+        #Chapter 2\
+        {\
+        'english':'1',\
+        'explanation':'',\
+        'korean':'하나',\
+        'audio_path':'audio/chapter2/01a.mp3'\
+        },{\
+        'english':'2',\
+        'explanation':'',\
+        'korean':'둘',\
+        'audio_path':'audio/chapter2/01b.mp3'\
+        },{\
+        'english':'3',\
+        'explanation':'',\
+        'korean':'셋',\
+        'audio_path':'audio/chapter2/01c.mp3'\
+        },{\
+        'english':'4',\
+        'explanation':'',\
+        'korean':'넷',\
+        'audio_path':'audio/chapter2/01d.mp3'\
+        },{\
+        'english':'5',\
+        'explanation':'',\
+        'korean':'다섯',\
+        'audio_path':'audio/chapter2/01e.mp3'\
+        },{\
+        'english':'6',\
+        'explanation':'',\
+        'korean':'여섯',\
+        'audio_path':'audio/chapter2/01f.mp3'\
+        },{\
+        'english':'7',\
+        'explanation':'',\
+        'korean':'일곱',\
+        'audio_path':'audio/chapter2/01g.mp3'\
+        },{\
+        'english':'8',\
+        'explanation':'',\
+        'korean':'여덟',\
+        'audio_path':'audio/chapter2/01h.mp3'\
+        },{\
+        'english':'9',\
+        'explanation':'',\
+        'korean':'아홉',\
+        'audio_path':'audio/chapter2/01i.mp3'\
+        },{\
+        'english':'10',\
+        'explanation':'',\
+        'korean':'열',\
+        'audio_path':'audio/chapter2/01j.mp3'\
+        },{\
+        'english':'Shall we start?',\
+        'explanation':'',\
+        'korean':'시작할까요?',\
+        'audio_path':'audio/chapter2/02.mp3'\
+        },{\
+        'english':'Let’s begin. (teacher to students)',\
+        'explanation':'',\
+        'korean':'시작합시다.',\
+        'audio_path':'audio/chapter2/03.mp3'\
+        },{\
+        'english':'Please look at your books.',\
+        'explanation':'Explanation: 책 book(s); 책을 book(s) (as direct object); 보세요 please look (at it).',\
+        'korean':'책을 보세요.',\
+        'audio_path':'audio/chapter2/04.mp3'\
+        },{\
+        'english':'Please look at the first page.',\
+        'explanation':'Explanation: 책 book(s); 책을 book(s)(as direct object); 보새요 please look (at it).',\
+        'korean':'첫 베이지를 보새요',\
+        'audio_path':'audio/chapter2/05.mp3'\
+        },{\
+        'english':'Please don’t look at your books.',\
+        'explanation':'',\
+        'korean':'책을 보지 마세요.',\
+        'audio_path':'audio/chapter2/06.mp3'\
+        },{\
+        'english':'Please look at the next page.',\
+        'explanation':'',\
+        'korean':'다음 베이지를 보세요.',\
+        'audio_path':'audio/chapter2/07.mp3'\
+        },{\
+        'english':'Just listen, please.',\
+        'explanation':'Explanation: 듣기 the act of listening; 듣기만 only listening; 하세요 please do it.',\
+        'korean':'듣기만 히세요.',\
+        'audio_path':'audio/chapter2/08.mp3'\
+        },{\
+        'english':'Please repeat (after me).',\
+        'explanation':'Explanation: 따라 following, repeating 하세요 please do, please say.',\
+        'korean':'따라 하세요.',\
+        'audio_path':'audio/chapter2/09.mp3'\
+        },{\
+        'english':'All together.',\
+        'explanation':'Explanation: 따라 all; everyone; 같이 together (pronounced 가치)',\
+        'korean':'다 같이.',\
+        'audio_path':'audio/chapter2/10.mp3'\
+        },{\
+        'english':'One more time.',\
+        'explanation':'Explanation: 다시 again, once more; 한번 once, one time',\
+        'korean':'다시 한번.',\
+        'audio_path':'audio/chapter2/11.mp3'\
+        },{\
+        'english':'Please answer.',\
+        'explanation':'',\
+        'korean':'대답하세요.',\
+        'audio_path':'audio/chapter2/12.mp3'\
+        },{\
+        'english':'Please say it or Please talk.',\
+        'explanation':'Explanation: 말 language, word(s), speech, talking; 말(을) 하세요 please speak; please say it. 말씀하세요 is even more polite.',\
+        'korean':'말하세요.',\
+        'audio_path':'audio/chapter2/13.mp3'\
+        },{\
+        'english':'Please say it for me again. Please repeat.',\
+        'explanation':'Explanation: 다시 agai, once more; 해주세요 Please favor me by doing it, i.e., Please do it for me.',\
+        'korean':'다시 말씀해 주세요.',\
+        'audio_path':'audio/chapter2/14.mp3'\
+        },{\
+        'english':'Please say it loudly.',\
+        'explanation':'Explanation: 끄게 so that it is big; loudly.',\
+        'korean':'끄게 말씀해 주세요',\
+        'audio_path':'audio/chapter2/15.mp3'\
+        },{\
+        'english':'Please say it slowly.',\
+        'explanation':'',\
+        'korean':'천천히 말씀하세요.',\
+        'audio_path':'audio/chapter2/16.mp3'\
+        },{\
+        'english':'Please say it in Korean.',\
+        'explanation':'Explanation: 한국말로 in Korean; 하세요 Please say it.',\
+        'korean':'한국말로 하세요.',\
+        'audio_path':'audio/chapter2/17.mp3'\
+        },{\
+        'english':'Please don’t say it in English.',\
+        'explanation':'Explanation: 영어로 in English; 하지 마세요 Please do not do it.',\
+        'korean':'영어로 하지 마세요.',\
+        'audio_path':'audio/chapter2/18.mp3'\
+        },{\
+        'english':'Do you understand? Literally: Will you know? Might you know? Would you know?',\
+        'explanation':'',\
+        'korean':'알겠어요?',\
+        'audio_path':'audio/chapter2/19.mp3'\
+        },{\
+        'english':'(Yes,) I understand. Literally: I will know. I would know. I’ll probably get it.',\
+        'explanation':'',\
+        'korean':'(네) 알겠어요.',\
+        'audio_path':'audio/chapter2/20.mp3'\
+        },{\
+        'english':'No, I don’t understand. Literally: I don’t or wouldn’t know.',\
+        'explanation':'',\
+        'korean':'아니오, 모르겠어요.',\
+        'audio_path':'audio/chapter2/21.mp3'\
+        },{\
+        'english':'Any questions?',\
+        'explanation':'Explanation: 있어요? Does there exist . . . ? or Do we/you have . . . ? 질문 question',\
+        'korean':'질문 있어요?',\
+        'audio_path':'audio/chapter2/22.mp3'\
+        },{\
+        'english':'Yes, I have (a question). Yes, there are.',\
+        'explanation':'',\
+        'korean':'네. 있어요.',\
+        'audio_path':'audio/chapter2/23.mp3'\
+        },{\
+        'english':'No, I haven’t.',\
+        'explanation':'Explanation: 없어요 There does not exist or I/we/one does not have.',\
+        'korean':'아니오. 없어요.',\
+        'audio_path':'audio/chapter2/24.mp3'\
+        },{\
+        'english':'Let’s rest for ten minutes.',\
+        'explanation':'Explanation: 십 ten (a different word, but with the same meaning as 열); 분 minute; 십분 10 minutes; 십분만 (to the extent of) 10 minutes; ten minutes (only); 쉽시다 Let’s rest.',\
+        'korean':'십분만 쉽시다.',\
+        'audio_path':'audio/chapter2/25.mp3'\
+        },{\
+        'english':'Sorry I’m late.',\
+        'explanation':'Explanation: 늦어서 I am late, and so . . . Because I am late, . . .',\
+        'korean':'늦어서 죄송합니다.',\
+        'audio_path':'audio/chapter2/.mp3'\
+        },{\
+        'english':'',\
+        'explanation':'',\
+        'korean':'',\
+        'audio_path':'audio/chapter2/.mp3'\
+        }\
+        ]]
