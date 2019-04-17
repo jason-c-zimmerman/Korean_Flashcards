7b5cddd8d2f0269ac26badb7fb61a7e7bdfef349
Jason C Zimmerman <jason.c.zimmerman@gmail.com>
2019-04-16 23:08:19 -0700

    added config script

---
+++ config.sh
@@ -0,0 +1,29 @@
+bash=`which bash`
+#!$bash
+
+haveProg() {
+	[ -x "$(which $1)" ]		
+}
+
+if haveProg apt-get; then pkgman="sudo apt-get"
+elif haveProg yum; then pkgman="sudo yum"
+elif haveProg pkg; then pkgman="pkg"
+else
+	echo 'No package manager found.'
+	exit 2
+fi
+
+python=`which python`
+if [ "$python" = "" ] 
+then
+	$pkgman install python
+fi
+
+if [ $SHELL = "/data/data/com.termux/files/usr/bin/bash" ] 
+then
+	pkg install play-audio
+fi
+
+pip install readchar
+pip install keyboard
+pip install playsound
