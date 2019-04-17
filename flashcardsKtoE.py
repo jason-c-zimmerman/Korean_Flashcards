645db40995c1485cdf2bee98180331f32f557a86
Jason C Zimmerman <jason.c.zimmerman@gmail.com>
2019-04-16 20:06:39 -0700

    removed wav, can play mp3 python. fixed bug in args

--- flashcardsKtoE.py
+++ flashcardsKtoE.py
@@ -15,57 +15,59 @@ import random
 from dictionary import dictionary
 from readchar import readkey
 import keyboard
+from playsound import playsound
 #import getopt #TODO make the args opts
 
 def main():
 	if len(sys.argv) < 2 or len(sys.argv) > 3:
-        usage()
-        sys.exit(2)
+		usage()
+		sys.exit(2)
 	SCREENWIDTH = 80 
-	if len(sys.argv) > 3 and sys.argv[2] = 'm':
-		SCREENWIDTH=24
-    os.system('clear')
-    os.system('stty -echo')
-    print('\n\n\n\033[1m' + 'Welcome to Korean Flashcards'.center(SCREENWIDTH) + '\033[0m\n\n\n\n\n') 
-    sys.stdout.write('\033[5m' + 'Press any key to continue.'.center(SCREENWIDTH) + '\033[0m')
-    sys.stdout.flush()
-    readkey()
-    keyin = ''
-    answer = True
-    while keyin != 'q':
-        if keyin == 'p':
-            keyin = ''
-            os.system('mpg123 -q ' + card['audio_path'])
-            keyin = readkey()
-        elif keyin == 'n' and answer == False:
-            answer = True
-            os.system('clear')
-            print('\n\n\n\n\n\n')
-            print(card['korean'].center(SCREENWIDTH))
-            print('\n\n\n')
-            print(card['english'].center(SCREENWIDTH))
-            if card['explanation'] != '':
-                print(card['explanation'].center(SCREENWIDTH))
-            print('\n\n\n\n\n')
-            sys.stdout.write('Press p to play audio, q to quit, and any other key to continue.')
-            sys.stdout.flush()
-            keyin = readkey()
-            if keyin == 'n':
-                keyin = ''
-        elif answer == True: 
-            answer = False
-            card = get_rand_card()
-            os.system('clear')
-            print('\n\n\n\n\n\n\n\n\n')
-            print(card['korean'].center(SCREENWIDTH))
-            print('\n\n\n\n\n')
-            sys.stdout.write('p to play audio, n to show answer, q to quit: '.center(SCREENWIDTH))
-            sys.stdout.flush()
-            keyin = readkey()
-        else:
-            keyin = readkey()
-    os.system('clear')
-    os.system('stty echo')
+	if len(sys.argv) == 3 and sys.argv[2] == 'm':
+		SCREENWIDTH = 24
+	os.system('clear')
+	os.system('stty -echo')
+	print('\n\n\n\033[1m' + 'Welcome to Korean Flashcards'.center(SCREENWIDTH) + '\033[0m\n\n\n\n\n') 
+	sys.stdout.write('\033[5m' + 'Press any key to continue.'.center(SCREENWIDTH) + '\033[0m')
+	sys.stdout.flush()
+	readkey()
+	keyin = ''
+	answer = True
+
+	while keyin != 'q':
+		if keyin == 'p':
+			keyin = ''
+			playsound(card['audio_path'])
+			keyin = readkey()
+		elif keyin == 'n' and answer == False:
+			answer = True
+			os.system('clear')
+			print('\n\n\n\n\n\n')
+			print(card['korean'].center(SCREENWIDTH))
+			print('\n\n\n')
+			print(card['english'].center(SCREENWIDTH))
+			if card['explanation'] != '':
+				print(card['explanation'].center(SCREENWIDTH))
+			print('\n\n\n\n\n')
+			sys.stdout.write('p audio, any, q quit')
+			sys.stdout.flush()
+			keyin = readkey()
+			if keyin == 'n':
+				keyin = ''
+		elif answer == True: 
+			answer = False
+			card = get_rand_card()
+			os.system('clear')
+			print('\n\n\n\n\n\n\n\n\n')
+			print(card['korean'].center(SCREENWIDTH))
+			print('\n\n\n\n\n')
+			sys.stdout.write('p audio, n ans, q quit'.center(SCREENWIDTH))
+			sys.stdout.flush()
+			keyin = readkey()
+		else:
+			keyin = readkey()
+	os.system('clear')
+	os.system('stty echo')
 
 def get_rand_card():
     # if sys.argv[1] == 'all':
